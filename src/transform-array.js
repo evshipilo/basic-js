56d9bc029e5ed1074972578722ebd3ca757b20f7
Yauheni Shypila <evshipilo@gmail.com>
2020-02-21 14:35:37 +0300

    fix: 5 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,35 @@
 module.exports = function transform(arr) {
-    //if (!arr.isArray) throw new Error('not array');
+    if (!(arr instanceof Array)) throw new Error('not array');
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == '--discard-next' && (i + 1) < arr.length) arr.splice(i + 1, 1);
-        if (arr[i] == '--discard-prev' && (i - 1) >= 0) {
-            arr.splice(i - 1, 1);
+        if (arr[i] == '--discard-next' && (i + 1) < arr.length) {
+            i++;
+            continue;
+        } else {
+            if (arr[i] == '--discard-prev' && (i - 1) >= 0) {
+                result.pop();continue;
+            } else {
+                if (arr[i] == '--double-next' && (i + 1) < arr.length){
+                    result.push(arr[i+1]);continue;
+                }
+                else {
+                    if (arr[i] == '--double-prev' && (i - 1) >= 0){
+                       result.push(arr[i-1]);continue;
+                    }
+                }
+            }
+        }
+        result.push(arr[i]);
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] == '--discard-next' ||
+            result[i] == '--discard-prev' ||
+            result[i] == '--double-next' ||
+            result[i] == '--double-prev') {
+            result.splice(i, 1);
             i--;
         }
-        if (arr[i] == '--double-next' && (i + 1) < arr.length) arr.splice(i + 1, 0, arr[i + 1]);
-        if (arr[i] == '--double-prev' && (i - 1) >= 0) {
-            arr.splice(i - 1, 0, arr[i - 1]);
-            i++;
-        }
-
     }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == '--discard-next' ||
-            arr[i] == '--discard-prev' ||
-            arr[i] == '--double-next' ||
-            arr[i] == '--double-prev') {
-            arr.splice(i,1); i--;
-        }
-    }
-    return arr;
+    return result;
 };
